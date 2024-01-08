e5497f2ffce5769cb926ac1c6d85b08df0635393
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-08 15:33:28 +0100

    3commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 const apiKey = "41ad82297cf0fd9fcb842c3a02bb8abd";
 
-
-const checkWeather = () => {
+function checkWeather() {
     const locationName = document.getElementById("locationInput").value;
-
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=${apiKey}`;
 
     fetch(apiUrl)
@@ -17,8 +15,7 @@ const checkWeather = () => {
         });
 }
 
-const displayWeather =(data) => {
-
+function displayWeather(data) {
     const locationNameElement = document.getElementById("locationName");
     const temperatureElement = document.getElementById("temperature");
     const weatherIconElement = document.getElementById("weatherIcon");
@@ -31,13 +28,12 @@ const displayWeather =(data) => {
     const sunsetElement = document.getElementById("sunset");
     const geoCoordsElement = document.getElementById("geoCoords");
 
-
     if (data && data.name && data.main && data.weather && data.sys && data.coord) {
-
-        locationNameElement.textContent = ` ${data.name}`;
+        locationNameElement.textContent = `Wetter in ${data.name}`;
 
         if (data.main.temp !== undefined) {
-            temperatureElement.innerHTML = `<div id="temperatureValue">${Math.round(data.main.temp - 273.15)}°C</div>`;
+            const temperatureValue = Math.round(data.main.temp - 273.15);
+            temperatureElement.innerHTML = `<div id="temperatureValue" style="font-size: ${calculateFontSize(temperatureValue)};">${temperatureValue}°C</div>`;
         } else {
             temperatureElement.innerHTML = `<div id="temperatureValue">Temperatur nicht verfügbar</div>`;
         }
@@ -45,37 +41,41 @@ const displayWeather =(data) => {
         const weatherDescription = data.weather[0].description.toLowerCase();
         weatherIconElement.innerHTML = getWeatherIcon(weatherDescription);
 
-        localTimeElement.textContent = `Local Time:${new Date(data.dt * 1000).toLocaleTimeString()}`;
-        windSpeedElement.textContent = `WindSpeed: ${data.wind ? data.wind.speed : 'N/A'} m/s`;
-        cloudinessElement.textContent = `Cloudiness: ${data.weather[0].description || 'N/A'}`;
-        pressureElement.textContent = `Pressure: ${data.main.pressure ? data.main.pressure + ' hPa' : 'N/A'}`;
-        humidityElement.textContent = `Humidity: ${data.main.humidity ? data.main.humidity + ' %' : 'N/A'}`;
-        sunriseElement.textContent = `Sunrise: ${data.sys.sunrise ? new Date(data.sys.sunrise * 1000).toLocaleTimeString() : 'N/A'}`;
-        sunsetElement.textContent = `Sunset: ${data.sys.sunset ? new Date(data.sys.sunset * 1000).toLocaleTimeString() : 'N/A'}`;
-        geoCoordsElement.textContent = `Geo Coords: [${data.coord.lat || 'N/A'}, ${data.coord.lon || 'N/A'}]`;
+        localTimeElement.textContent = `Lokalzeit: ${new Date(data.dt * 1000).toLocaleTimeString()}`;
+        windSpeedElement.textContent = `Windgeschwindigkeit: ${data.wind ? data.wind.speed : 'N/A'} m/s`;
+        cloudinessElement.textContent = `Bewölkung: ${data.weather[0].description || 'N/A'}`;
+        pressureElement.textContent = `Luftdruck: ${data.main.pressure ? data.main.pressure + ' hPa' : 'N/A'}`;
+        humidityElement.textContent = `Luftfeuchtigkeit: ${data.main.humidity ? data.main.humidity + ' %' : 'N/A'}`;
+        sunriseElement.textContent = `Sonnenaufgang: ${data.sys.sunrise ? new Date(data.sys.sunrise * 1000).toLocaleTimeString() : 'N/A'}`;
+        sunsetElement.textContent = `Sonnenuntergang: ${data.sys.sunset ? new Date(data.sys.sunset * 1000).toLocaleTimeString() : 'N/A'}`;
+        geoCoordsElement.textContent = `Geografische Koordinaten: [${data.coord.lat || 'N/A'}, ${data.coord.lon || 'N/A'}]`;
     } else {
         console.error("Ungültige oder unvollständige Datenstruktur in der API-Antwort.");
         alert("Ungültige oder unvollständige Datenstruktur in der API-Antwort.");
     }
 }
 
-const getWeatherIcon = (description) => {
+function getWeatherIcon(description) {
     const lowercaseDescription = description.toLowerCase();
 
     if (lowercaseDescription.includes('sun') || lowercaseDescription.includes('clear sky') || lowercaseDescription.includes('haze') || lowercaseDescription.includes('smoke')) {
-        return '<img src="./assets/img/sonnig.png" alt="Sonnig" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/sonnig.png" alt="Sonnig" style="width: 100px; height: 100px;">';
     } else if (lowercaseDescription.includes('few clouds') || lowercaseDescription.includes('overcast') || lowercaseDescription.includes('broken clouds')) {
-        return '<img src="./assets/img/wolkig.png" alt="Wolkig" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/wolkig.png" alt="Wolkig" style="width: 100px; height: 100px;">';
     } else if (lowercaseDescription.includes('overcast clouds') || lowercaseDescription.includes('scattered clouds')) {
-        return '<img src="./assets/img/teilSonneTeilewolkeregen.png" alt="Teilweise Sonne, Teile Wolken, Regen" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/teilSonneTeilewolkeregen.png" alt="Teilweise Sonne, Teile Wolken, Regen" style="width: 100px; height: 100px;">';
     } else if (lowercaseDescription.includes('ice')) {
-        return '<img src="./assets/img/eissig.png" alt="Eisig" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/eissig.png" alt="Eisig" style="width: 100px; height: 100px;">';
     } else if (lowercaseDescription.includes('rain')  || lowercaseDescription.includes('light rain')) {
-        return '<img src="./assets/img/regen.png" alt="Regen" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/regen.png" alt="Regen" style="width: 100px; height: 100px;">';
     } else if (lowercaseDescription.includes('heavy snow')) {
-        return '<img src="./assets/img/schnee.png" alt="Schnee" style="width: 50px; height: 50px;">';
+        return '<img src="./assets/img/schnee.png" alt="Schnee" style="width: 100px; height: 100px;">';
     } else {
         return description;
     }
+}
+
+function calculateFontSize(temperature) {
+    return `${Math.max(20, 40 - temperature)}px`;
 }
 
